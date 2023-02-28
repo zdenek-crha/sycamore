214fe8c377b085dbc1bcab6cda91558fdafd499e
Zdenek Crha <zdenek.crha@gmail.com>
2023-02-28 21:39:12 +0100

    Document example on attributes passthrough

---
+++ examples/attributes-passthrough/README.md
@@ -0,0 +1,5 @@
+# Collect and pass dynamic attributes to inner element
+
+This example shows how to setup component props `struct` so that all
+unknown/dynamic attributes are collected in single field. This collection can
+then be passed to inner element(s). Either as is or with some modifications.
--- examples/attributes-passthrough/src/main.rs
+++ examples/attributes-passthrough/src/main.rs
@@ -2,8 +2,14 @@ use sycamore::prelude::*;
 
 #[derive(Props)]
 pub struct AccessibleInputProps<'cx, G: Html> {
+
+    // component attribute, will be handled localy and *not* passed-thorugh
     label_id: &'static str,
+
+    // collection of passthrough attributes
     attributes: Attributes<'cx, G>,
+
+    // collection of passthrough children nodes/components
     children: Children<'cx, G>,
 }
 
@@ -12,11 +18,17 @@ fn AccessibleSearchBox<'cx, G: Html>(
     cx: Scope<'cx>,
     props: AccessibleInputProps<'cx, G>,
 ) -> View<G> {
+
+    // we remove attributes that are handled by component and should not be passed through
     props
         .attributes
         .exclude_keys(&["aria-role", "aria-labelledby"]);
+
+    // render all child nodes into temporary variable
     let children = props.children.call(cx);
 
+    // render component as `input` node using remaining attributes and wrapping rendered children
+    // nodes.
     view! { cx,
         input(aria-role = "searchbox", aria-labelledby = props.label_id, ..props.attributes) {
             (children)
@@ -30,6 +42,7 @@ fn App<G: Html>(cx: Scope) -> View<G> {
         div {
             "Passthrough attributes demo"
 
+            // attributes prefixed with attr: will be accumulated into `attributes` field
             label(id = "searchbox1_label") { "Search Box 1" }
             AccessibleSearchBox(label_id = "searchbox1_label", attr:style="background-color:slategray;") {}
             label(id = "searchbox2_label") { "Search Box 2" }
