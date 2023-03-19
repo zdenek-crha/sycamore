5fc35b91cc480a071f39e01e9aa3793079b89e22
Zdenek Crha <zdenek.crha@gmail.com>
2023-03-19 16:13:40 +0100

    wip: mess with dynamic hydration

--- examples/ssr-hydrate/app/src/lib.rs
+++ examples/ssr-hydrate/app/src/lib.rs
@@ -7,11 +7,35 @@ pub struct AppProps<'cx, G: Html> {
 
 #[component]
 pub fn App<'cx, G: Html>(cx: Scope<'cx>, props: AppProps<'cx, G>) -> View<G> {
-    let children = props.children.call(cx);
 
-    view! { cx, div {
-        (children)
-    }}
+    #[cfg(feature="hydrate")]
+    {
+        use std::any::TypeId;
+        if TypeId::of::<G>() == TypeId::of::<HydrateNode>() {
+            log::debug!("hydrate app");
+            while let Some(el) =  sycamore::web::hydrate::get_next_element() {
+                log::debug!("found el");
+                if let Some(pair) =  sycamore::utils::hydrate::get_current_id() {
+                    log::debug!("{:?}", pair);
+                }
+            }
+            view!{cx, "foo"}
+        }
+        else {
+            let children = props.children.call(cx);
+            view! { cx, div {
+                (children)
+            }}
+        }
+    }
+
+    #[cfg(not(feature="hydrate"))]
+    {
+        let children = props.children.call(cx);
+        view! { cx, div {
+            (children)
+        }}
+    }
 }
 
 
@@ -26,6 +50,8 @@ pub fn Counter<G: Html>(cx: Scope, props: CounterProps) -> View<G> {
     let state = create_signal(cx, props.value);
     let increment = |_| state.set(*state.get() + 1);
     let decrement = |_| state.set(*state.get() - 1);
+
+    log::debug!("hydrate counter");
 
     view! { cx,
         div {
